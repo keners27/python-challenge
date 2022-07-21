d6ae21cb950aa50967d4902211183453f659b2a2
keners27 <dank@iinet.net.au>
2022-07-21 16:50:28 +0800

    pybank complete

--- PyBank/main.py/pyscript.py.py
+++ PyBank/main.py/pyscript.py.py
@@ -1,6 +1,7 @@
 import os
 import csv
 
+
 pybank_csv = os.path.join('..', 'Resources', 'budget_data.csv')
 
 date = []
@@ -23,18 +24,40 @@ with open(pybank_csv) as csvfile:
         monthly_profit.append(total_profit[i+1]-total_profit[i])
         
 
-max_increase_value = max(monthly_profit)
-max_decrease_value = min(monthly_profit)
+increase = max(monthly_profit)
+decrease = min(monthly_profit)
 
-max_increase_month = monthly_profit.index(max(monthly_profit)) + 1
-max_decrease_month = monthly_profit.index(min(monthly_profit)) + 1 
+max_increase = monthly_profit.index(max(monthly_profit)) + 1
+max_decrease = monthly_profit.index(min(monthly_profit)) + 1 
 
 print("Financial Analysis")
 print("--------------------------------------")
 print(f"Total Months: {len(date)}")
 print(f"Total: ${sum(total_profit)}")
 print(f"Average Change: {round(sum(monthly_profit)/len(monthly_profit),2)}")
-print(f"Greatest Increase in Profits: {date[max_increase_month]} (${(str(max_increase_value))})")
-print(f"Greatest Decrease in Profits: {date[max_decrease_month]} (${(str(max_decrease_value))})")
+print(f"Greatest Increase in Profits: {date[max_increase]} (${(str(increase))})")
+print(f"Greatest Decrease in Profits: {date[max_decrease]} (${(str(decrease))})")
+
+
+from pathlib import Path 
+output_file = Path("..", "analysis", "finance.txt")
+
+with open(output_file,"w") as f:
+
+    f.write("Financial Analysis")
+    f.write("\n")
+    f.write("--------------------------------------")
+    f.write("\n")
+    f.write(f"Total Months: {len(date)}")
+    f.write("\n")
+    f.write(f"Total: ${sum(total_profit)}")
+    f.write("\n")
+    f.write(f"Average Change: {round(sum(monthly_profit)/len(monthly_profit),2)}")
+    f.write("\n")
+    f.write(f"Greatest Increase in Profits: {date[max_increase]} (${(str(increase))})")
+    f.write("\n")
+    f.write(f"Greatest Decrease in Profits: {date[max_decrease]} (${(str(decrease))})")
+
+
 
 
